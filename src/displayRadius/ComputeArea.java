34a67a82e080f6ec5b06ce5580c17868eadfad82
mehrbarz <mehrbarzegar@gmail.com>
2021-04-29 22:37:01 +0200

    area

---
+++ src/displayRadius/ComputeArea.java
@@ -0,0 +1,16 @@
+package displayRadius;
+
+public class ComputeArea {
+
+	public static void main(String[] args) {
+		double radius;
+		double area;
+		
+		radius = 20;
+		area = radius * radius * 3.14159;
+		System.out.println("The area for the cricle of radius "+
+		radius + "is "+ area);
+
+	}
+
+}
